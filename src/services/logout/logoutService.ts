5cf82b660d09c0ae9b7e4e75cc27e3fe6ed86c1d
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2024-02-19 23:19:47 -0300

    feat: logout service

---
+++ src/services/logout/logoutService.ts
@@ -0,0 +1,34 @@
+import { IUserRepository } from "repositories/IUserRepository";
+
+interface ILogoutParams {
+  email: string;
+  sessionToken: string;
+}
+
+class LogoutService {
+  constructor(public userRepository: IUserRepository) {}
+
+  logoutUser = async ({ email, sessionToken }: ILogoutParams) => {
+    try {
+      if (!email) {
+        throw new Error("O e-mail é obrigatório");
+      }
+      if (!sessionToken) {
+        throw new Error("o session token é obrigatório");
+      }
+
+      const currentUser = await this.userRepository.findByEmail(email);
+      if (!currentUser) {
+        throw new Error("Usuário não encontrado");
+      }
+
+      const user = await this.userRepository.logout(email, sessionToken);
+
+      return user;
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+}
+
+export { LogoutService };

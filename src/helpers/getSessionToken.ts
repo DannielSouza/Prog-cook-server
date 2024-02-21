295cabf44c7e085dc01f8c2d1197e7bc37eb2626
Daniel  Souza <104663666+DannielSouza@users.noreply.github.com>
2024-02-20 22:15:14 -0300

    fix: delete log

--- src/helpers/getSessionToken.ts
+++ src/helpers/getSessionToken.ts
@@ -2,7 +2,6 @@ import { Request } from "express";
 
 export const getSessionToken = (req: Request) => {
   const sessionToken = req.cookies["COOK-AUTH"];
-  console.log(JSON.stringify(req.cookies));
 
   if (!sessionToken) {
     throw new Error("Token inválido");
